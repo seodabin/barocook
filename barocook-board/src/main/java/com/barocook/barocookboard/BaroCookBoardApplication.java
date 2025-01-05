e0256653573f99ace688e7eb22c5ce83f9c73219
seodabin <seodb89@gmail.com>
2025-01-05 14:45:54 +0900

    #5 -  스프링 부트 프로젝트 시작
    * 스프링 웹
    * 엑츄레이터
    * 데브툴즈
    * 롬복

---
+++ barocook-board/src/main/java/com/barocook/barocookboard/BaroCookBoardApplication.java
@@ -0,0 +1,13 @@
+package com.barocook.barocookboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaroCookBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaroCookBoardApplication.class, args);
+    }
+
+}

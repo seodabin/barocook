396add3533fe4b0f74b83cf5d2998c82d43961a5
seodabin <seodb89@gmail.com>
2025-01-26 12:21:50 +0900

    #10 - datasouce 암호화
    jasypt 사용 / 추후 외부 환경변수 주입으로 변경 고려

---
+++ barocook-board/src/test/java/com/barocook/barocookboard/config/JasyptConfigTest.java
@@ -0,0 +1,39 @@
+package com.barocook.barocookboard.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Properties;
+
+import static org.assertj.core.api.Assertions.*;
+
+class JasyptConfigTest {
+    @Test
+    void stringEncryptor() {
+        String url = "";
+        String username = "";
+        String password = "";
+
+        System.out.println(jasyptEncoding(url));
+        System.out.println(jasyptEncoding(username));
+        System.out.println(jasyptEncoding(password));
+    }
+
+    public String jasyptEncoding(String value) {
+
+        String key = "";
+        StandardPBEStringEncryptor pbeEnc = new StandardPBEStringEncryptor();
+        pbeEnc.setAlgorithm("PBEWITHHMACSHA512ANDAES_256");
+        pbeEnc.setPassword(key);
+        pbeEnc.setIvGenerator(new RandomIvGenerator());
+        return pbeEnc.encrypt(value);
+    }
+}
